a1e6cd14246dabf34927fc4acc45b0b411d3d20d
johnpoint <jahanngauss414@gmail.com>
2018-03-31 16:15:46 +0800

    # update

--- bot/push.py
+++ bot/push.py
@@ -3,15 +3,28 @@
 
 import os
 import config
-import caozuo
 import time
 import telebot
-from telebot import types
+import ping
 
 TOKEN = config.TOKENP
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def bot_start(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, '欢迎~我可以为你监控服务器在线状态哟 戳 /help 查看帮助！')
+fs = open("chatid",'r')
+id = fs.read()
+fs.close()
+f = open("db.py",'r')
+s = f.readlines()
+for ip in s:
+    r = ping.check_ip_ping(ip)
+    if r == 0:
+        pass
+    elif r == 1:
+        bot.send_chat_action(message.chat.id,'typing')
+        bot.send_message(id,u'⚠⚠ %s 异常'%ip)
+    else:
+        bot.send_message(message.chat.id,'Error')
+f.close()
+os._exit()
+
+
