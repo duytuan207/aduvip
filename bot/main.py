cb5d454cd0fe18e1824c6e5df8006a0242f57d75
duytuan207 <88073690+duytuan207@users.noreply.github.com>
2023-09-15 11:35:18 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -20,7 +20,7 @@ def bot_start(message):
 @bot.message_handler(commands=['help'])
 def bot_help(message):
     bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id,'我能帮你监控服务器状态哟 \n /add 添加服务器 \n /del 删除服务器 \n /list 查看服务器列表 \n /test 查询在线情况')
+    bot.send_message(message.chat.id,'Tôi có thể giúp bạn theo dõi trạng thái máy chủ \n /add Thêm máy chủ \n /del Xóa máy chủ \n /list Xem danh sách máy chủ \n /test Kiểm tra trạng thái trực tuyến')
 
 @bot.message_handler(commands=['add'])
 def bot_add(message):
@@ -28,14 +28,14 @@ def bot_add(message):
     f = open('admin', 'r')
     l = f.read()
     if l.find('%s' %un) == -1:
-        bot.reply_to(message,'为了保护隐私，本bot仅限个人使用')
+        bot.reply_to(message,'Để bảo vệ quyền riêng tư, bot này chỉ được giới hạn cho mục đích sử dụng cá nhân')
     else:
         if len(message.text.split(' ')) == 2:
             r = caozuo.add_server(message.text.split(' ')[1])
             bot.send_chat_action(message.chat.id,'typing')
             bot.send_message(message.chat.id,r)
         else:
-            bot.send_chat_action(message.chat.id,'参数错误')
+            bot.send_chat_action(message.chat.id,'Lỗi tham số')
 
 @bot.message_handler(commands=['del'])
 def bot_del(message):
@@ -50,7 +50,7 @@ def bot_del(message):
             bot.send_chat_action(message.chat.id,'typing')
             bot.send_message(message.chat.id,r)
         else:
-            bot.send_chat_action(message.chat.id,'参数错误')
+            bot.send_chat_action(message.chat.id,'Lỗi tham số')
 
 @bot.message_handler(commands=['list'])
 def bot_list(message):
@@ -58,12 +58,12 @@ def bot_list(message):
     f = open('admin', 'r')
     l = f.read()
     if l.find('%s' %un) == -1:
-        bot.reply_to(message,'为了保护隐私，本bot仅限个人使用')
+        bot.reply_to(message,'Để bảo vệ quyền riêng tư, bot này chỉ được giới hạn cho mục đích sử dụng cá nhân')
     else:
         f = open("db.py",'r')
         s = f.read()
         bot.send_chat_action(message.chat.id,'typing')
-        bot.send_message(message.chat.id,'监控列表： \n%s'%s)
+        bot.send_message(message.chat.id,'Danh sách giám sát： \n%s'%s)
         f.close()
         
 @bot.message_handler(commands=['test'])
@@ -72,7 +72,7 @@ def bot_test(message):
     f = open('admin', 'r')
     l = f.read()
     if l.find('%s' %un) == -1:
-        bot.reply_to(message,'为了保护隐私，本bot仅限个人使用')
+        bot.reply_to(message,'Để bảo vệ quyền riêng tư, bot này chỉ được giới hạn cho mục đích sử dụng cá nhân')
     else:
         f = open("db.py",'r')
         s = f.readlines()
@@ -80,10 +80,10 @@ def bot_test(message):
             r = ping.check_ip_ping(ip)
             if r == 0:
                 bot.send_chat_action(message.chat.id,'typing')
-                bot.send_message(message.chat.id,u'%s 正常'%ip)
+                bot.send_message(message.chat.id,u'%s Bình thường'%ip)
             elif r == 1:
                 bot.send_chat_action(message.chat.id,'typing')
-                bot.send_message(message.chat.id,u'⚠⚠ %s 异常'%ip)
+                bot.send_message(message.chat.id,u'⚠⚠ %s Bất thường'%ip)
             else:
                 bot.send_message(message.chat.id,'Error')
         f.close()
@@ -95,27 +95,27 @@ def bot_lock(message):
 	if r == False:
 	    if r == False:
 		    os.mknod("admin")
-		    bot.send_message(message.chat.id,'已创建绑定id')
+		    bot.send_message(message.chat.id,'Id ràng buộc đã được tạo')
 	    else:
 		    pass
-		    bot.send_message(message.chat.id,'绑定id已存在，pass')
+		    bot.send_message(message.chat.id,'Id liên kết đã tồn tại，pass')
 	    if rdb == False:
 		    os.mknod("db.py")
-		    bot.send_message(message.chat.id,'已创建ip库')
+		    bot.send_message(message.chat.id,'Thư viện IP đã được tạo')
 	    else:
 		    pass
-		    bot.send_message(message.chat.id,'ip库文件已存在，pass')
-	    msg_id = bot.send_message(message.chat.id,'绑定用户id…').message_id
+		    bot.send_message(message.chat.id,'Tệp thư viện ip đã tồn tại，pass')
+	    msg_id = bot.send_message(message.chat.id,'Liên kết id người dùng…').message_id
 	    uid = message.from_user.id
 	    f = open("admin",'w')
 	    i = uid
 	    print >> f,i
 	    f.close()
-	    bot.edit_message_text('绑定完成！', message.chat.id, msg_id)
+	    bot.edit_message_text('Ràng buộc hoàn thành！', message.chat.id, msg_id)
 	    time.sleep(10)
-	    bot.edit_message_text('配置完成~', message.chat.id, msg_id)
+	    bot.edit_message_text('Cấu hình hoàn tất~', message.chat.id, msg_id)
 	else:
-		bot.send_message(message.chat.id,'已经绑定过！？')
+		bot.send_message(message.chat.id,'Đã bị ràng buộc！？')
 
 def bot_warn():
     fs = open("admin",'r')
@@ -128,9 +128,9 @@ def bot_warn():
             pass
         elif r == 1:
             bot.send_chat_action(id,'typing')
-            bot.send_message(id,u'[PUSH]⚠⚠警报 ⚠⚠ \n%s 出现异常 \n**本警报由bot自动发出**'%ip)
+            bot.send_message(id,u'[PUSH]⚠⚠警报 ⚠⚠ \n%s 出现异常 \n**Cảnh báo này được bot tự động đưa ra**'%ip)
         else:
-            bot.send_message(message.chat.id,'Error:未知错误，请向 @johnpoint 反应')
+            bot.send_message(message.chat.id,'Error:Lỗi không xác định, xin vui lòng hỏi facebook.com/ndtdzhuhu')
     f.close()
 
 if __name__ == '__main__':
